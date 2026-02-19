4addb63968dfc1d9e542448578cc35402875e9f8
Baek001 <__>
2026-02-20 02:19:12 +0900

    Force env variables in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   devIndicators: {
     appIsrStatus: false,
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
-
 export default nextConfig;
